2c29a8ec722e4a3392dd3156d244f956de3b9184
WorkStation <mg.mg.ghostrecon7@mail.ru>
2021-10-20 10:46:04 +0300

    Lesson4

---
+++ src/main/java/Lesson_4/ChatApp.java
@@ -0,0 +1,62 @@
+package Lesson_4;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Создать окно для клиентской части чата: большое текстовое поле для отображения переписки в центре окна.
+ * Однострочное текстовое поле для ввода сообщений и кнопка для отсылки сообщений на нижней панели.
+ * Сообщение должно отсылаться либо по нажатию кнопки на форме, либо по нажатию кнопки Enter.
+ * При «отсылке» сообщение перекидывается из нижнего поля в центральное.
+ */
+
+public class ChatApp extends JFrame {
+
+    public ChatApp(){
+        setTitle("Chat");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setBounds(100,0,400,615);
+        setLayout(null);
+
+        JTextArea messagesField = new JTextArea();
+        messagesField.setBounds(0, 0, 400, 400);
+        add(messagesField);
+
+        JButton sendMessageButton = new JButton("Отправить сообщение");
+        sendMessageButton.setBounds(100, 415, 200, 50);
+        add(sendMessageButton);
+
+
+        JTextField userMessageField = new JTextField();
+        userMessageField.setBounds(0, 480, 400,100);
+        add(userMessageField);
+
+        sendMessageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                messagesField.setText(messagesField.getText()+ "\n" +userMessageField.getText());
+                userMessageField.setText("");
+            }
+        });
+
+        userMessageField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                messagesField.setText(messagesField.getText()+ "\n" +userMessageField.getText());
+                userMessageField.setText("");
+            }
+        });
+
+
+
+
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ChatApp();
+    }
+
+}
