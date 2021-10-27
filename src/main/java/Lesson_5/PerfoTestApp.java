ed3fd56bf9c3220fc929c4e4fec08ebbc4e6ddbb
WorkStation <mg.mg.ghostrecon7@mail.ru>
2021-10-27 14:09:26 +0300

    Lesson4

--- src/main/java/Lesson_5/PerfoTestApp.java
+++ src/main/java/Lesson_5/PerfoTestApp.java
@@ -1,7 +1,7 @@
 package Lesson_5;
 
 public class PerfoTestApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         firstMethod();
         secondMethod();
     }
@@ -19,11 +19,11 @@ public class PerfoTestApp {
         System.out.println("One thread time: " + (System.currentTimeMillis() - startTime) + " ms.");
     }
 
-    public static void secondMethod(){
-        int size = 160;
+    public static void secondMethod() throws InterruptedException {
+        int size = 10_000_000;
         float[] arr = new float[size];
         for (int i = 0; i < arr.length; i++){
-            arr[i] = arr[i-1] + 1.0f;
+            arr[i] = 1.0f;
         }
 
         long startTime = System.currentTimeMillis();
@@ -33,16 +33,26 @@ public class PerfoTestApp {
         System.arraycopy(arr, 0, copyArrLeft, 0, size/2);
         System.arraycopy(arr, size/2, copyArrRight, 0, size/2);
 
-        System.out.println(copyArrLeft.length + " / " + copyArrRight.length);
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int leftArrCounter = 0; leftArrCounter < copyArrLeft.length; leftArrCounter++){
-                copyArrLeft[i] = (float) (copyArrLeft[i] * Math.sin(0.2f + i / 5) *
-                        Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < copyArrLeft.length; i++) {
+                    copyArrLeft[i] = (float) (copyArrLeft[i] * Math.sin(0.2f + i / 5) *
+                            Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
             }
+        });
 
-        }
+        Thread thread2 = new Thread(() -> {
+            for (int i = 0, j = copyArrLeft.length; i < copyArrRight.length; j++, i++) {
+                copyArrRight[i] = (float) (copyArrRight[i] * Math.sin(0.2f + j / 5) *
+                        Math.cos(0.2f + j / 5) * Math.cos(0.4f + j / 2));
+            }
+        });
 
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
         System.out.println("Two thread time: " + (System.currentTimeMillis() - startTime) + " ms.");
     }
 }
